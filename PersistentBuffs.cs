bf0a396fddf98cc6d54d199ee136978cca3b2f2d
Aloe <162494218+aceynk@users.noreply.github.com>
2024-06-28 15:14:56 -0700

    Fixed many issues with food buffs, and effects adding upon base bonuses.

--- PersistentBuffs.cs
+++ PersistentBuffs.cs
@@ -1,8 +1,11 @@
-﻿using Force.DeepCloner;
+﻿using System.Text.Json;
+using Microsoft.Xna.Framework;
 using Netcode;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
+using StardewValley.Buffs;
+using StardewValley.GameData.Buffs;
 
 namespace PersistentBuffs;
 
@@ -15,11 +18,11 @@ public class ModEntry : Mod
     
     public static IMonitor _log = null!;
     //public NetStringList DayEndBuffs = null!;
-    //public Dictionary<string, int> DayEndBuffRemaining = new();
+    //public Dictionary<string, int> DayEndBuffData = new();
 
-    public string DEBname = "aceynk.PersistentBuffs.DayEndBuffs";
-    public string DEBRname = "aceynk.PersistentBuffs.DayEndBuffRemaining";
+    public string BDname = "aceynk.PersistentBuffs.BuffData";
 
+    /*
     public NetString CondenseBuffs(NetStringList value)
     {
         if (value == new NetStringList())
@@ -44,7 +47,7 @@ public class ModEntry : Mod
         return output;
     }
 
-    public NetString CondenseBuffRemaining(Dictionary<string, int> value)
+    public NetString CondenseBuffData(Dictionary<string, int> value)
     {
         if (value == new Dictionary<string, int>())
         {
@@ -56,15 +59,16 @@ public class ModEntry : Mod
         return new NetString(string.Join("/", output));
     }
 
-    public Dictionary<string, int> DecodeBuffRemaining(NetString value)
+    
+    public Dictionary<string, int> oDecodeBuffData(NetString value)
     {
         if (value == new NetString())
         {
-            return new Dictionary<string, int>();
+            return new Dictionary<string, Dictionary<string, string>>();
         }
         
         List<string> itemList = value.ToString().Split("/").ToList();
-        Dictionary<string, int> output = new();
+        Dictionary<string, Dictionary<string, string>> output = new();
 
         foreach (string item in itemList)
         {
@@ -73,6 +77,151 @@ public class ModEntry : Mod
             int val = int.Parse(item.Split(":")[1]);
 
             output[key] = val;
+        }
+
+        return output;
+    }
+    */
+    
+    private NetString EncodeBuffData(Dictionary<string, Buff> value)
+    {
+        Dictionary<string, Dictionary<string, object>> simplifiedBuffs = new();
+
+        foreach (KeyValuePair<string, Buff> pair in value)
+        {
+            Buff tObj = pair.Value;
+            Dictionary<string, object> newDict
+                = new();
+            newDict["DisplayName"] = tObj.displayName;
+            newDict["Description"] = tObj.description;
+            newDict["GlowColor"] = tObj.glow.PackedValue;
+            newDict["Duration"] = tObj.totalMillisecondsDuration;
+            newDict["FarmingLevel"] = tObj.effects.FarmingLevel.Value;
+            newDict["FishingLevel"] = tObj.effects.FishingLevel.Value;
+            newDict["MiningLevel"] = tObj.effects.MiningLevel.Value;
+            newDict["LuckLevel"] = tObj.effects.LuckLevel.Value;
+            newDict["ForagingLevel"] = tObj.effects.ForagingLevel.Value;
+            newDict["MaxStamina"] = tObj.effects.MaxStamina.Value;
+            newDict["MagneticRadius"] = tObj.effects.MagneticRadius.Value;
+            newDict["Speed"] = tObj.effects.Speed.Value;
+            newDict["Defense"] = tObj.effects.Defense.Value;
+            newDict["Attack"] = tObj.effects.Attack.Value;
+            
+            newDict["ActionsOnApply"] = tObj.actionsOnApply;
+            newDict["Source"] = tObj.source;
+            newDict["DisplaySource"] = tObj.displaySource;
+            newDict["Id"] = tObj.id;
+
+            simplifiedBuffs[tObj.id] = newDict;
+        }
+        
+        string output = JsonSerializer.Serialize(simplifiedBuffs);
+        
+        return new NetString(output);
+    }
+
+    private Dictionary<string, Buff> DecodeBuffData(NetString value)
+    {
+        
+        
+        Dictionary<string, Dictionary<string, object>>? input =
+            JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, object>>>(value);
+        Dictionary<string, Buff> output = new();
+        
+        if (input == null) return new Dictionary<string, Buff>();
+
+        foreach (KeyValuePair<string, Dictionary<string, object>> pair in input)
+        {
+            Dictionary<string, object> buffDict = pair.Value;
+
+            BuffAttributesData bAttr = new();
+            
+            // EFFECTS ADDS TO BASE EFFECT VALUES
+            Buff tentativeBuff = new Buff(pair.Key);
+            
+            float tryDeserializeFloat(string name)
+            {
+                if (!buffDict.ContainsKey(name))
+                {
+                    return 0;
+                }
+
+                if (buffDict[name] == null)
+                {
+                    return 0;
+                }
+
+                float output = ((JsonElement)buffDict[name]).Deserialize<float>();
+                
+                return output;
+            }
+            
+            float ProcAttr(string name)
+            {
+                if (buffDict.ContainsKey(name))
+                {
+                    
+                    return tryDeserializeFloat(name);
+                }
+
+                return (float)0;
+            }
+            
+            bAttr.FarmingLevel = ProcAttr("FarmingLevel") - tentativeBuff.effects.FarmingLevel.Value;
+            bAttr.FishingLevel = ProcAttr("FishingLevel") - tentativeBuff.effects.FishingLevel.Value;
+            bAttr.MiningLevel = ProcAttr("MiningLevel") - tentativeBuff.effects.MiningLevel.Value;
+            bAttr.LuckLevel = ProcAttr("LuckLevel") - tentativeBuff.effects.LuckLevel.Value;
+            bAttr.ForagingLevel = ProcAttr("ForagingLevel") - tentativeBuff.effects.ForagingLevel.Value;
+            bAttr.MaxStamina = ProcAttr("MaxStamina") - tentativeBuff.effects.MaxStamina.Value;
+            bAttr.MagneticRadius = ProcAttr("MagneticRadius") - tentativeBuff.effects.MagneticRadius.Value;
+            bAttr.Speed = ProcAttr("Speed") - tentativeBuff.effects.Speed.Value;
+            bAttr.Defense = ProcAttr("Defense") - tentativeBuff.effects.Defense.Value;
+            bAttr.Attack = ProcAttr("Attack") - tentativeBuff.effects.Attack.Value;
+            
+            string tryDeserializeString(string name)
+            {
+                if (!buffDict.ContainsKey(name))
+                {
+                    return "";
+                }
+
+                if (buffDict[name] == null)
+                {
+                    return "";
+                }
+
+                return ((JsonElement)buffDict[name]).Deserialize<string>();
+            }
+
+            int tryDeserializeInt(string name)
+            {
+                if (!buffDict.ContainsKey(name))
+                {
+                    return 0;
+                }
+
+                if (buffDict[name] == null)
+                {
+                    return 0;
+                }
+
+                return ((JsonElement)buffDict[name]).Deserialize<int>();
+            }
+            
+            Buff newBuff = new Buff(
+                tryDeserializeString("Id"),
+                tryDeserializeString("Source"),
+                tryDeserializeString("DisplaySource"),
+                duration: tryDeserializeInt("Duration"),
+                effects: new BuffEffects(bAttr),
+                displayName: tryDeserializeString("DisplayName"),
+                description: tryDeserializeString("Description")
+            );
+
+            newBuff.actionsOnApply = (string[])buffDict["ActionsOnApply"];
+            newBuff.glow = new Color((uint)tryDeserializeInt("GlowColor"));
+
+            output[pair.Key] = newBuff;
         }
 
         return output;
@@ -96,22 +245,16 @@ public class ModEntry : Mod
 
     private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
     {
-        if (!Game1.player.modData.Keys.Contains(DEBname))
+        if (!Game1.player.modData.Keys.Contains(BDname))
         {
-            Game1.player.modData[DEBname] = new NetString();
-        }
-
-        if (!Game1.player.modData.Keys.Contains(DEBRname))
-        {
-            Game1.player.modData[DEBRname] = new NetString();
+            Game1.player.modData[BDname] = new NetString();
         }
     }
 
-    private void OnDayEnding(object? sender, DayEndingEventArgs e)
+    private void OnDayEnding(object? sender, DayEndingEventArgs e) 
     {
-        NetStringList DayEndBuffs = DecodeBuffs(new NetString(Game1.player.modData[DEBname]));
-        Dictionary<string, int> DayEndBuffRemaining =
-            DecodeBuffRemaining(new NetString(Game1.player.modData[DEBRname]));
+        Dictionary<string, Buff> DayEndBuffData =
+            DecodeBuffData(new NetString(Game1.player.modData[BDname]));
         
         NetStringList curBuffIds = Game1.player.buffs.AppliedBuffIds;
         Dictionary<string, bool> protectedIdsDict = Game1.content.Load<Dictionary<string, bool>>("aceynk.PersistentBuffs/PersistentBuffIds");
@@ -120,53 +263,49 @@ public class ModEntry : Mod
         
         List<string> curBuffIdsList = curBuffIds.Where(v => protectedIds.Contains(v)).ToList();
 
+        int foodCount = 0;
+        
         foreach (string buffId in curBuffIdsList)
         {
-            DayEndBuffRemaining[buffId] = Game1.player.buffs.AppliedBuffs[buffId].millisecondsDuration;
+            if (buffId == "food")
+            {
+                DayEndBuffData["food" + foodCount] = Game1.player.buffs.AppliedBuffs[buffId];
+                foodCount++;
+                
+                continue;
+            }
+            
+            DayEndBuffData[buffId] = Game1.player.buffs.AppliedBuffs[buffId];
         }
-
-        DayEndBuffs = curBuffIds.DeepClone();
-
-        Game1.player.modData[DEBname] = CondenseBuffs(DayEndBuffs);
-        Game1.player.modData[DEBRname] = CondenseBuffRemaining(DayEndBuffRemaining);
+        
+        Game1.player.modData[BDname] = EncodeBuffData(DayEndBuffData);
     }
 
     private void OnDayStarted(object? sender, DayStartedEventArgs e)
     {
-        NetStringList DayEndBuffs = new();
-        Dictionary<string, int> DayEndBuffRemaining = new();
+        Dictionary<string, Buff> DayEndBuffData = new();
         
         try
         {
-            DayEndBuffs = DecodeBuffs(new NetString(Game1.player.modData[DEBname]));
-            DayEndBuffRemaining =
-                DecodeBuffRemaining(new NetString(Game1.player.modData[DEBRname]));
+            DayEndBuffData =
+                DecodeBuffData(new NetString(Game1.player.modData[BDname]));
         }
-        catch
+        catch (Exception Ex)
         {
             Log("Failed to fetch modData on day start.");
-        }
-
-        if (DayEndBuffs == null)
-        {
-            return;
+            Log(Ex.ToString());
         }
         
-        if (DayEndBuffs != new NetStringList())
+        foreach (Buff thisBuff in DayEndBuffData.Values)
         {
-            foreach (string buffId in DayEndBuffs)
-            {
-                if (buffId == "") continue;
-                Buff thisBuff = new Buff(buffId, duration: DayEndBuffRemaining[buffId]);
-                Game1.player.applyBuff(thisBuff);
-            }
+            
+            
+            Game1.player.applyBuff(thisBuff);
         }
 
-        DayEndBuffs.Clear();
-        DayEndBuffRemaining.Clear();
+        DayEndBuffData.Clear();
         
-        Game1.player.modData[DEBname] = CondenseBuffs(DayEndBuffs);
-        Game1.player.modData[DEBRname] = CondenseBuffRemaining(DayEndBuffRemaining);
+        Game1.player.modData[BDname] = EncodeBuffData(DayEndBuffData);
     }
 
     private static void OnAssetRequested(object? sender, AssetRequestedEventArgs e)
